916561505b55aaba79c6bd24c5cf869fc6c386d0
Surya <surya.thiru001@gmail.com>
2018-02-02 16:43:20 +0530

    qr gen with ws

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const crypto = require('crypto');
 const QRcode = require('qrcode');
 const app = require('express')();
 const http = require('http').Server(app);
+const io = require('socket.io')(http);
 const path = require('path');
+
+let id;
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
@@ -14,6 +17,34 @@ app.get('/qr', function(req, res) {
     QRcode.toDataURL(id, (err, url) => {
     res.send(url);
     });
+});
+
+app.get('/validateqr/:id', function(req, res) {
+    if (!id) {
+        res.send('wait in line. no id yet');
+    }
+    else {
+        if (id == req.params.id) {
+            res.send('you are validated');
+        }
+        else {
+            res.send('nice try. get out');
+        }
+    }
+});
+
+io.on('connection', function(socket) {
+    console.log('oh look a user!!');
+
+    function sendQR() {
+        id = crypto.randomBytes(8).toString('hex');
+
+        QRcode.toDataURL(id, (err, url) => {
+            socket.emit('qrcode', url);
+        });
+    }
+
+    setInterval(sendQR, 10000);
 });
 
 const port = 8080;
